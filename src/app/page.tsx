53986d60c374f702ed742639bc1c67ddbf321510
Jean-Louis <dev02@sportyneo.com>
2025-02-04 12:23:41 +0100

    Test Ansible

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <div className="text-white">
         <Header/>
         <main className="snap-container">
-          <Background src="/background.jpg" title="Cachemire"/>
+          <Background src="/background.jpg" title="Cachemire + Git Push + Check Ansible"/>
           <Background src="/hybrideground.jpg" title="Manteau Hybride"/>
           <Background src="/doudouneground.jpg" title="Doudounes"/>
         </main>
