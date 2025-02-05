b03683d892718d1b773c629ed79f9755cd122329
Jean-Louis <dev02@sportyneo.com>
2025-02-05 10:35:29 +0100

    Deploy Success pls

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <div className="text-white">
         <Header/>
         <main className="snap-container">
-          <Background src="/background.jpg" title="Cachemire + 2nd Test Deploy"/>
+          <Background src="/background.jpg" title="Cachemire + Deploy Should Work"/>
           <Background src="/hybrideground.jpg" title="Manteau Hybride"/>
           <Background src="/doudouneground.jpg" title="Doudounes"/>
         </main>
